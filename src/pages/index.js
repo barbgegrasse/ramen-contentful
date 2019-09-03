141417bdfb7440110fb723338ca92a5ada7e67ca
Barbegrasse <johan@developpeur-web.tech>
2019-09-03 13:34:47 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allContentfulArticle.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="All posts" />
+        <Bio />
+        {posts.map(({ node }) => {
+          console.log(node);
+          
+          const title = node.titre || node.slug
+          return (
+            <article key={node.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.slug}>
+                    {title}
+                  </Link>
+                </h3>
+              </header>
+              <section>
+                  lorem ipsum
+              </section>
+            </article>
+            
+          )
+       
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+
+
+    allContentfulArticle {
+      edges {
+        node {
+          slug
+          titre
+          description {
+            content {
+              content {
+                value
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
