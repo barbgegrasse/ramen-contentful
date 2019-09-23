666d19f0acfe2e6d896f7a65a86af2a7a95caa66
Barbegrasse <johan@developpeur-web.tech>
2019-09-23 18:57:38 +0200

    test mobile animation

--- src/pages/index3.js
+++ src/pages/index3.js
@@ -1,56 +1,45 @@
 import React, {Component} from "react"
-import ReactDOM from 'react-dom';
-import ReactFullpage from '@fullpage/react-fullpage';
-import { Tween, Timeline } from 'react-gsap';
-import Lanterne from '../components/images/Lanterne.js'
 
 
-const Fullpage = () => (
-  <ReactFullpage
-/*
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
-*/
 
-    //fullpage options
-    licenseKey = {'6AA6C984-A7A24515-ABE38707-85B93007'}
-    scrollingSpeed = {500} /* Options here */
 
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <Lanterne />
-          </div>
-          <div className="section">
-            <section className="panel half-width left">
-               lorem ipsum gauche 
-            </section>
-            <section className="panel half-width right">
-               lorem ipsum droite
-            </section>
-          </div>
-          <div className="section">
-            <section className="panel half-width left">
-               lorem ipsum gauche 
-            </section>
-            <section className="panel half-width right">
-               lorem ipsum droite
-            </section>
+class Index3 extends Component {
 
-            <section className="panel half-width left">
-               test
-            </section>
-            <section className="panel half-width right">
-               test
-            </section>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
-);
+    componentDidMount(){
+        
+        let scrolling = false;
+        window.addEventListener('wheel', function(e) {
+            scrolling = true
+        });
+        setInterval( function() {
+            if ( scrolling ) {
+                scrolling = false;
+                const element = document.querySelector('.panel.active');
+                element.classList.remove('active');
+                element.nextSibling.classList.add('active');
+                
+            }
+        }, 500 );
+    }
 
-export default Fullpage;
+    render(){
+        return(
+            <>
+                <div className="kovsky-anim">
+                    <div className="panel max active">
+                        Panel max
+                    </div>
+                    <div className="panel half-width left">
+                        Panel Half width
+                    </div>
+                    <div className="panel half-width right">
+                        Panel Half width
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+
+export default Index3;
